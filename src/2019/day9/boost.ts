85c1db4d30e231dbe9b4a06ca7641185fe0ab9ce
Jonah Scheinerman <jonah@scheinerman.net>
2019-12-12 00:04:13 -0500

    Day 9 part 2

--- src/2019/day9/boost.ts
+++ src/2019/day9/boost.ts
@@ -1,6 +1,5 @@
 import { execute } from "../intcode/execute";
 import { boostProgram } from "./input";
 
-const result = execute(boostProgram, [1]);
-console.log("OUTPUT", result.output);
-console.log(boostProgram[53])
+const result = execute(boostProgram, [2]);
+console.log(result.output);
