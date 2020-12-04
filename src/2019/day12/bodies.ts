75a2adf21c9e6b3b32e541c43b5896c4fb3d9e8b
Jonah Scheinerman <jonah@scheinerman.net>
2020-12-04 00:51:10 -0500

    Who knows

--- src/2019/day12/bodies.ts
+++ src/2019/day12/bodies.ts
@@ -5,17 +5,17 @@ interface Moon {
 
 type Coordinate = [number, number, number];
 
-function sum(values: number[]) {
-    return values.reduce((l, r) => l + r, 0);
-}
+// function sum(values: number[]) {
+//     return values.reduce((l, r) => l + r, 0);
+// }
 
-function getCoordinateEnergy(coordinate: Coordinate) {
-    return sum(coordinate.map(Math.abs));
-}
+// function getCoordinateEnergy(coordinate: Coordinate) {
+//     return sum(coordinate.map(Math.abs));
+// }
 
-function getMoonEnergy(moon: Moon) {
-    return getCoordinateEnergy(moon.position) * getCoordinateEnergy(moon.velocity);
-}
+// function getMoonEnergy(moon: Moon) {
+//     return getCoordinateEnergy(moon.position) * getCoordinateEnergy(moon.velocity);
+// }
 
 function zip(left: Coordinate, right: Coordinate, zipper: (left: number, right: number) => number) {
     return left.map((l, i) => zipper(l, right[i])) as Coordinate;
@@ -56,18 +56,51 @@ function step(moons: Moon[]) {
 }
 
 export function simulate(positions: Coordinate[], steps: number) {
+    // const initialMoons = positions
+    //     .map((position): Moon => ({ position, velocity: [0, 0, 0] }))
+    //     .map(moon => JSON.stringify(moon));
     const moons = positions.map((position): Moon => ({ position, velocity: [0, 0, 0] }));
+
+    const visitedStates = positions.map(() => new Map<string, number>());
+    const cycleLengths = moons.map(() => new Map<number, number>());
     for (let s = 0; s < steps; s++) {
+        for (let i = 0; i < moons.length; i++) {
+            const moon = moons[i];
+            const serializedMoon = JSON.stringify(moon);
+            const previousIndex = visitedStates[i].get(serializedMoon);
+            if (previousIndex !== undefined) {
+                const length = s - previousIndex;
+                const count = cycleLengths[i].get(length) || 0;
+                cycleLengths[i].set(length, count + 1);
+            }
+
+            visitedStates[i].set(serializedMoon, s);
+        }
+
         step(moons);
     }
-    console.log(sum(moons.map(getMoonEnergy)));
+    console.log(cycleLengths);
 }
 
+// const initialPositions: Coordinate[] = [
+//     [-1, 0, 2], //
+//     [2, -10, -7], //
+//     [4, -8, 8], //
+//     [3, 5, -1], //
+// ];
+
 const initialPositions: Coordinate[] = [
-    [3, 2, -6], //
-    [-13, 18, 10], //
-    [-8, -1, 13], //
-    [5, 10, 4], //
+    [-8, -10, 0], //
+    [5, 5, 10], //
+    [2, -7, 3], //
+    [9, -8, -3], //
 ];
 
-simulate(initialPositions, 1000);
+// const initialPositions: Coordinate[] = [
+//     [3, 2, -6], //
+//     [-13, 18, 10], //
+//     [-8, -1, 13], //
+//     [5, 10, 4], //
+// ];
+
+simulate(initialPositions, 1000000);
--- src/2019/day13/arcade.ts
+++ src/2019/day13/arcade.ts
@@ -1,8 +1,5 @@
-import { execute } from "../intcode/execute";
+import { executeProgram, getInitialProgramState } from "../intcode/execute";
 import { arcadeCode } from "./arcadeCode";
-
-arcadeCode[0] = 2;
-const { output } = execute(arcadeCode);
 
 enum Tile {
     EMPTY = 0,
@@ -20,41 +17,78 @@ const TILE_CHARS: Record<Tile, string> = {
     [Tile.BALL]: "o",
 };
 
-interface TilePlacement {
-    row: number;
-    column: number;
-    tile: Tile;
-}
+function createGrid(rows: number, columns: number) {
+    const grid: Tile[][] = [];
 
-const tiles: TilePlacement[] = [];
-for (let index = 0; index < output.length; index += 3) {
-    const [column, row, tile] = output.slice(index, index + 3);
-    tiles.push({ row, column, tile: tile as Tile });
-}
-
-const rowValues = tiles.map(tile => tile.row);
-const colValues = tiles.map(tile => tile.column);
-
-const rows = Math.max(...rowValues) + 1;
-const columns = Math.max(...colValues) + 1;
-const grid: Tile[][] = [];
-
-for (let r = 0; r < rows; r++) {
-    const row: Tile[] = [];
-    for (let c = 0; c < columns; c++) {
-        row.push(Tile.EMPTY);
+    for (let r = 0; r < rows; r++) {
+        const row: Tile[] = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(Tile.EMPTY);
+        }
+        grid.push(row);
     }
-    grid.push(row);
+
+    return grid;
 }
 
-for (const { row, column, tile } of tiles) {
-    grid[row][column] = tile;
-}
-
-for (const r of grid) {
-    let row = "";
-    for (const c of r) {
-        row += TILE_CHARS[c];
+function printGrid(grid: Tile[][]) {
+    for (const r of grid) {
+        let row = "";
+        for (const c of r) {
+            row += TILE_CHARS[c];
+        }
+        console.log(row);
     }
-    console.log(row);
 }
+
+interface GameState {
+    score: number;
+    grid: Tile[][];
+}
+
+function handleOutput(state: GameState, output: number[]) {
+    for (let index = 0; index < output.length; index += 3) {
+        const [x, y, arg] = output.slice(index, index + 3);
+        if (x === -1) {
+            state.score = arg;
+        } else {
+            state.grid[y][x] = arg as Tile;
+        }
+    }
+}
+
+function findX(grid: Tile[][], tile: Tile) {
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            if (grid[y][x] === tile) {
+                return x;
+            }
+        }
+    }
+    return -1;
+}
+
+function runGame(code: number[]) {
+    code[0] = 2;
+    let computerState = getInitialProgramState(code, []);
+    const gameState: GameState = {
+        score: 0,
+        grid: createGrid(23, 44),
+    };
+
+    while (!computerState.halted) {
+        computerState = executeProgram(computerState);
+        handleOutput(gameState, computerState.output);
+        computerState.output = [];
+
+        printGrid(gameState.grid);
+
+        const paddleX = findX(gameState.grid, Tile.PADDLE);
+        const ballX = findX(gameState.grid, Tile.BALL);
+        computerState.input = [Math.sign(ballX - paddleX)];
+    }
+
+    console.log(gameState.score)
+}
+
+runGame(arcadeCode);
