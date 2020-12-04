850b33dc8c144c3fc51e7624eab2b1afe2e6d140
Jonah Scheinerman <jonah@scheinerman.net>
2020-12-04 08:23:23 -0500

    2020 day 1 part 2

--- src/2020/day1/expense-report.ts
+++ src/2020/day1/expense-report.ts
@@ -1,15 +1,26 @@
 import { readFileSync } from "fs";
 
-const result = readFileSync("src/2020/day1/input.txt", { encoding: "utf-8" })
+const values = readFileSync("src/2020/day1/input.txt", { encoding: "utf-8" })
     .split("\n")
     .map(line => parseInt(line, 10));
 
-for (let i = 0; i < result.length; i++) {
-    for (let j = i + 1; j < result.length; j++) {
-        const a = result[i];
-        const b = result[j];
+const pairwiseSums = new Map<number, [number, number]>();
+for (let i = 0; i < values.length; i++) {
+    for (let j = i + 1; j < values.length; j++) {
+        const a = values[i];
+        const b = values[j];
+        pairwiseSums.set(a + b, [a, b]);
         if (a + b === 2020) {
             console.log(a * b);
         }
+    }
+}
+
+for (const value of values) {
+    const expectedSum = 2020 - value;
+    if (pairwiseSums.has(expectedSum)) {
+        const [a, b] = pairwiseSums.get(expectedSum)!;
+        console.log(a * b * value);
+        break;
     }
 }
